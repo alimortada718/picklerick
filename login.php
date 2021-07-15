ea3cae7f1ae445e3b1568d22f84a85892bffdc3c
alimortada718 <79694567+alimortada718@users.noreply.github.com>
2021-07-15 03:24:10 +0300

    3

--- login.php
+++ login.php
@@ -6,31 +6,33 @@ if ($_SERVER['request_method'] == "POST")
     //something was posted
     if (!empty($user_name) && !empty($password)) {
         //read from database
+        if ($_POST) {
+            //making sure that SQL INJECTION doesn't work
+            $user_name = mysqli_real_escape_string($conn, $user_name); //test or 1=1
+            $password = mysqli_real_escape_string($conn, $password);
+        }
+
+
+
         $query = " SELECT * FROM users WHERE user_name='$user_name' limit 1";
-      $result =  mysqli_query($con, $query);
-      if($result)
-{        if ($result && mysqli_num_rows($result) > 0) {
-    $user_data = mysqli_fetch_assoc($result);
-    if($user_data['password']=== $_password)
-    {$_SESSION['user_id']=$user_data['user_id'];
-        header("location: index.php");
+        $result =  mysqli_query($con, $query);
+        if ($result) {
+            if ($result && mysqli_num_rows($result) > 0) {
+                $user_data = mysqli_fetch_assoc($result);
+                if ($user_data['password'] === $_password) {
+                    $_SESSION['user_id'] = $user_data['user_id'];
+                    header("location: index.php");
 
-        die;
-    }
-}
-
-
-
-}
+                    die;
+                }
+            }
+        }
 
 
 
 
 
-echo "PLEASE enter some valid information!";
-
-
-
+        echo "PLEASE enter some valid information!";
     } else {
         echo "PLEASE enter some valid information!";
     }
