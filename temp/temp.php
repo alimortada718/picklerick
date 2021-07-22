4676f2b9a7fea464866b0662f08c417d21f7ab94
alimortada718 <79694567+alimortada718@users.noreply.github.com>
2021-07-22 20:14:31 +0300

    4

--- temp/temp.php
+++ temp/temp.php
@@ -1,81 +1,59 @@
-<?php
+<!<!DOCTYPE html>
+    <html>
 
-session_start();
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <title></title>
+        <meta name="description" content="" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="shortcut icon" type="image/jpg" href="" />
+        <!-- favicon -->
+        <link rel="stylesheet" href="" />
+    </head>
 
-if ($_SESSION["loggedin"] != 1) {
-    header("location:index.php");
-}
-//  DONT FORGET TO CLOSE YOU LINE ;
-include "includes/db.php";
-include "function.php";
+    <body>
 
-$sql = "SELECT date,username,safe1,safe2,scratch1,scratch2 ,tobacco1,tobacco2 FROM `report`";
+        <header>
+            <div class="container"><img src="" alt="" /></div>
+        </header>
+        <div class="container">
 
-$result =  mysqli_query($conn, $sql);
-$report = mysqli_fetch_all($result);
+            <table>
+                <tr>
+                    <th>sugma</th>
+                    <th>deez</th>
+                </tr>
+                <tr>
+                    <td>cell1 row</td>
+                    <td></td>
 
-$label = array('date', 'username', 'safe1', 'safe2', 'scratch1', 'scratch2 ', 'tobacco1', 'tobacco2');
 
-// echo "<pre>";
-// //var_dump($report);
-// echo "</pre>";
+                </tr>
 
-?>
 
-<!DOCTYPE html>
-<html>
-<header>
-    <link rel="stylesheet" href="css/style.css">
-</header>
-<div class="body">
-    <div class="all">
-        <h3 style="background-color:yellow;">Shelf</h3> -->
-        <?php
 
-        label($label);
 
-        ?>
-    </div>
-    <div class="all">
 
-        <?php
-        label($row);
-        ?>
-    </div>
+                </tr>
 
-    <div class="all">
-        <h3 style="background-color:powderblue;">Tobacco</h3>
 
-        <?php
-        label($label2);
-        ?>
-    </div>
-    <div class="all">
 
-        <?php
 
-        label($row2);
-        ?>
-    </div>
 
-    <div class="all">
-        <h3 style="background-color:green;">SAFE</h3>
 
-        <?php
-        label($label3);
 
-        ?>
-    </div>
-    <div class="all">
 
-        <?php
-        label($row3);
-        ?>
-    </div>
-    <div id="thesign">
-        <a href="signup.php">SIGNUP</a>
-    </div>
 
-</div>
+            </table>
 
-</html>
+
+
+
+
+
+
+        </div>
+    </body>
+
+    </html>
