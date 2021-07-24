4094799c15ecea24890cca5da1b80b6c029b84c4
alimortada718 <79694567+alimortada718@users.noreply.github.com>
2021-07-24 07:34:51 +0300

    1

--- temp/temp.php
+++ temp/temp.php
@@ -1,59 +1,8 @@
-<!<!DOCTYPE html>
-    <html>
+<?php
+$number = 1;
+while ($number == 1) {
+    echo "hi";
+    $number += 1;
+}
 
-    <head>
-        <meta charset="utf-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <title></title>
-        <meta name="description" content="" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="shortcut icon" type="image/jpg" href="" />
-        <!-- favicon -->
-        <link rel="stylesheet" href="" />
-    </head>
-
-    <body>
-
-        <header>
-            <div class="container"><img src="" alt="" /></div>
-        </header>
-        <div class="container">
-
-            <table>
-                <tr>
-                    <th>sugma</th>
-                    <th>deez</th>
-                </tr>
-                <tr>
-                    <td>cell1 row</td>
-                    <td></td>
-
-
-                </tr>
-
-
-
-
-
-                </tr>
-
-
-
-
-
-
-
-
-
-            </table>
-
-
-
-
-
-
-
-        </div>
-    </body>
-
-    </html>
+$sql = "SELECT date,username,shift,safe1,safe2,scratch1,scratch2 ,tobacco1,tobacco2 FROM `report` WHERE date ='$date'";
